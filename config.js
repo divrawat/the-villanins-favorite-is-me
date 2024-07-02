16ebee67b6b39990feeed722600a99e307b98912
divrawat <divrawat2001@gmail>
2024-07-02 21:48:58 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,219 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.thevillaninsfavoriteisme.in.net';
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>The Villanins Favourite is Me</u></a> is a romantic fantasy manga that follows the story of a young woman who finds herself reincarnated into a novel as a minor character. In this new world, she catches the eye of the story's villain, a powerful and enigmatic duke. Despite his fearsome reputation, the villain becomes captivated by her charm and kindness `;
+
+export const MANGA_NAME = 'The Villanins Favourite is Me';
+export const MANGA_AUTHOR = 'Sanha,Yunon';
+export const MANGA_RELEASE = '2023';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = ' Gyeonghyeon';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Drama, Fantasy, Manhwa, Romance';
+
+export const CHAPTER_PREFIX = 'the-villanins-favorite-is-me-chapter';
+export const IMAGE_PREFIX = 'images/the-villanins-favorite-is-me';
+export const NEXT_PREVIOUS_PREFIX = 'manga/the-villanins-favorite-is-me-chapter';
+export const URL_PREFIX = 'manga/the-villanins-favorite-is-me-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/the-villanins-favorite-is-me-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] rounded-full w-[72px] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'The Villanins Favourite is Me';
+export const NavbarName = "The Villanins Favourite is Me";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "The Villain's Favorite is Me is a captivating romantic fantasy manga that tells the story of Elena, a young woman who unexpectedly finds herself reincarnated into the world of a novel she once read. To her surprise, she is not the heroine but a minor character destined for a tragic end. Determined to change her fate, Elena decides to use her knowledge of the story to navigate her new life."
+    },
+    {
+        id: 2,
+        content: "Elena soon catches the attention of the novel's villain, Duke Lucian von Bismarck. Known for his cold and ruthless demeanor, Lucian is feared by many. However, Elena's genuine kindness and unique perspective begin to melt his icy exterior. Intrigued by her unusual behavior and unaffected by his fearsome reputation, Lucian starts to favor Elena, protecting her from the dangers of the world."
+    },
+    {
+        id: 3,
+        content: "As Elena and Lucian's relationship deepens, she learns that there is more to him than the villainous facade he presents. Lucian harbors deep wounds from his past, and Elena's compassion and understanding help him confront his inner demons. Together, they navigate the complexities of their world, facing political intrigue, power struggles, and the ever-present threat of those who wish to harm them."
+    },
+    {
+        id: 4,
+        content: "Elena's knowledge of the novel's plot proves invaluable, allowing her to anticipate and thwart various schemes. However, she soon realizes that her presence has altered the course of events, making the future uncertain. With Lucian by her side, Elena must find a way to secure their happiness while protecting those they care about."
+    },
+    {
+        id: 5,
+        content: "Throughout the story, Elena's character growth is evident as she evolves from a frightened girl into a strong and determined woman. Her relationship with Lucian blossoms into a deep and abiding love, transcending the roles they were assigned in the original narrative. Together, they defy the expectations of their world, proving that even the most unlikely of individuals can find redemption and happiness."
+    },
+    {
+        id: 6,
+        content: "The Villain's Favorite is Me masterfully blends elements of romance, fantasy, and adventure, creating a richly detailed world where characters are more than they seem. It explores themes of fate, choice, and the transformative power of love, delivering a story that is both heartwarming and thrilling."
+    }
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran Manga',
+        link: 'https://www.frierenmanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 13
+    }
+]
